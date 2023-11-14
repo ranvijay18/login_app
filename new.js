703d2a719f90c14cfe669b202f78019bb80a617c
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2023-11-14 20:58:30 +0530

    Create new.js

---
+++ new.js
@@ -0,0 +1,5 @@
+var head = document.getElementById("main-header");
+var head1 = document.getElementById("header-title");
+head.style.borderBottom = "solid 3px #000";
+var items = document.getElementsByClassName("title");
+items.style.fontWeight = "bold";
