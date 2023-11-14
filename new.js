fb00499e169761771144e4f1f8af5b54780016a8
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2023-11-14 21:37:30 +0530

    Update new.js

--- new.js
+++ new.js
@@ -5,3 +5,8 @@ var add = document.getElementsByClassName("title");
 add.style.fontWeight = "bold";
 var items = document.getElementsByClassName("list-group-item");
 items[2].style.backgroundColor = "yellow";
+
+var item = document.querySelector(".list-group-item:nth-child(1)");
+item.style.backgroundColor = "green";
+var item3 = document.querySelector(".list-group-item:nth-child(2)");
+item3.style.display = "none";
