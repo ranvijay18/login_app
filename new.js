deae0736afea26500684d9c3f0bbbe87425c6645
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2023-11-14 21:25:58 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,5 +1,7 @@
 var head = document.getElementById("main-header");
 var head1 = document.getElementById("header-title");
 head.style.borderBottom = "solid 3px #000";
-var items = document.getElementsByClassName("title");
-items.style.fontWeight = "bold";
+var add = document.getElementsByClassName("title");
+add.style.fontWeight = "bold";
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = "yellow";
